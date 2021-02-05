d7e92f89e7c79893ea8a21abba3938df14f44cf7
vh181 <dinizvictorhugo18@gmail.com>
2021-02-05 17:02:31 -0300

    2

--- app/src/main/java/com/gv/recycle/RepositoryLisActivity.kt
+++ app/src/main/java/com/gv/recycle/RepositoryLisActivity.kt
@@ -2,10 +2,17 @@ package com.gv.recycle
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.gv.recycle.data.fakeRepository
+import com.gv.recycle.model.RepositoryAdapter
 
 class RepositoryLisActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.repository_list_activity)
+        var recyclerViewRepositoryActivity: RecyclerView = findViewById(R.id.recycler_view_main)
+        recyclerViewRepositoryActivity.adapter = RepositoryAdapter(fakeRepository())
+        recyclerViewRepositoryActivity.layoutManager = LinearLayoutManager(this)
     }
 }
